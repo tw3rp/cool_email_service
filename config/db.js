14f0d1bc8d0ad3f92b6cb0c0b49c47f775369e3d
tw3rp <rahuldprabhu@gmail.com>
2015-07-29 18:25:10 -0500

    initialized angular

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
+var mongoose = require('mongoose');
+var models = require('../models/emaillist');
 
+var conn = mongoose.connection;
+var uri='mongodb://localhost/emaillist';
+
+conn.on('error', console.error.bind(console,"mongoose connection error"));
+
+conn.once('open',function(){
+		console.log("connected to "+ uri +"database");
+});
+
+mongoose.connect(uri);
+module.exports = conn;
---
+++ public/app.js
@@ -0,0 +1,3 @@
+var app = angular.module('email',[]);
+app.controller('emailController',function(){
+		});
