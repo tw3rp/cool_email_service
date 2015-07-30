1bf47f1c64998e95b3e1d288f88719e0e10c4788
tw3rp <rahuldprabhu@gmail.com>
2015-07-30 14:25:45 -0500

    created a bootstrapinput form

--- models/emaillist.js
+++ models/emaillist.js
@@ -4,7 +4,7 @@ var Schema =mongoose.Schema;
 var emailSchema= Schema({
 		email: {type:String, required:true, default:''},
 		firstname: {type:String, required:false, default:''},
-		lastname: {type:String, required:false, default:''}
+		message: {type:String, required:true, default:''}
 		});
 var Email = mongoose.model('EMAIL',emailSchema);
 module.exports= Email;
