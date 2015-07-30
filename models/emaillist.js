2d085936878eea8b45d9907428e5b9f0c6ba0519
tw3rp <rahuldprabhu@gmail.com>
2015-07-30 15:48:54 -0500

    added a server for talking to python engine

--- models/emaillist.js
+++ models/emaillist.js
@@ -3,8 +3,8 @@ var Schema =mongoose.Schema;
 
 var emailSchema= Schema({
 		email: {type:String, required:true, default:''},
-		firstname: {type:String, required:false, default:''},
-		message: {type:String, required:true, default:''}
+	//	firstname: {type:String, required:false, default:''},
+	//	message: {type:String, required:true, default:''}
 		});
 var Email = mongoose.model('EMAIL',emailSchema);
 module.exports= Email;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var EMAIL = require('../models/emaillist');
+var server = require('./server.js');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('*', function(req, res, next) {
   res.sendfile('../public/index.html');
+});
+
+router.post('/api/save', function(req,res){
+		console.log(req.body); 
+		EMAIL.create({email:req.body.email});
+		server.sendEmail();	
+		res.send("was saved");		
+
 });
 
 module.exports = router;
